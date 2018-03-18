501f3f6778b7cdb9bb5cbb2aa0c0e7bb61f14653
Guus van der Ham <2374868v@student.gla.ac.uk>
2018-03-18 14:05:10 +0000

    delete second app

--- rate_yo_uni_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RateYoUniAppConfig(AppConfig):
-    name = 'rate_yo_uni_app'
--- rate_yo_uni_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from rate_yo_uni_app import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-]
