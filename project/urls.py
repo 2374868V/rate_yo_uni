2ef3541ccb536dcfbe1e178bc265b645b219bc63
ZC44 <2227781C@student.gla.ac.uk>
2018-03-22 01:59:38 +0000

    C3

--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add_toilet/$', views.add_toilet, name='add_toilet'),
     url(r'^sign_up/', views.sign_up, name='sign_up'),
-    url(r'^show_toilet/', views.show_toilet, name='show_toilet')
+    url(r'^show_toilet/(?P<bathroomSlug>[\w\-]+)/$', views.show_toilet, name='show_toilet')
 ]
--- project/views.py
+++ project/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from project.models import Bathroom, BathroomInteraction
+from project.models import *
 from project.forms import BathroomForm
 # Create your views here.
-def index(request):
 
-    bathroom_list = Bathroom.objects.order_by('-rating')
+
+def index(request):
+    bathroom_list = Bathroom.objects.values_list('name', 'bathroomSlug',)
     context_dict = {'bathrooms': bathroom_list}
     return render(request, 'project/home.html', context_dict)
 
+
 def sign_up(request):
     return render(request, 'project/sign_up.html')
+
 
 def add_toilet(request):
     form = BathroomForm
@@ -18,11 +20,16 @@ def add_toilet(request):
         form = BathroomForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return(index(request))
+            return index(request)
         else:
             print(form.errors)
     return render(request, 'project/add_toilet.html', {'form': form})
 
-def show_toilet(request):
-    return render(request, 'project/show_toilet.hmtl')
+
+def show_toilet(request, bathroomSlug):
+    try:
+        context = {'toilet': Bathroom.objects.all().get(bathroomSlug=bathroomSlug)}
+    except:
+        context = {}
+    return render(request, 'project/show_toilet.html', context)
 
